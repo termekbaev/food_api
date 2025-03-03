3e438ee43f2cddc66a92159429afdeef6581ece6
Timur <t-ermekbaev@mail.ru>
2025-03-03 18:59:03 +0300

    Created query

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from rest_framework.generics import ListAPIView
+from .models import FoodCategory
+from .serializers import FoodListSerializer
 
-# Create your views here.
+
+class FoodCategoryListView(ListAPIView):
+    serializer_class = FoodListSerializer
+
+    def get_queryset(self):
+        queryset = FoodCategory.objects.annotate(num_foods=Count('food', filter=Q(food__is_publish=True))).filter(num_foods__gt=0)
+        return queryset
